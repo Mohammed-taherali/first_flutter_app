aa23d4ed9414346afeb1b663517c8386b0e43ee1
Mohammed-taherali <mohammedtaherali5253@gmail.com>
2024-01-05 23:27:17 +0530

    learning rows, columns, buttons, text field

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,21 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'new App, understood?',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
-        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'This is my new title, gotcha?'),
+    return const MaterialApp(
+      title: "App Title Goes here",
+      home: MyHomePage(title: "Very new good title"),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -56,134 +24,245 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  String _error_msg = "";
-
-  void _incrementCounter() {
-    if (_counter == 0) {
-      setState(() {
-        _error_msg = "";
-      });
-    }
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void someFunc() {
+    print("Button pressed");
   }
 
-  void _decrementCounter() {
-    if (_counter == 0) {
-      setState(() {
-        _error_msg = "Counter cannot be less than zero!";
-      });
-    } else {
-      setState(() {
-        // This call to setState tells the Flutter framework that something has
-        // changed in this State, which causes it to rerun the build method below
-        // so that the display can reflect the updated values. If we changed
-        // _counter without calling setState(), then the build method would not be
-        // called again, and so nothing would appear to happen.
-        _counter--;
-      });
-    }
+  void longFunc() {
+    print("Long press");
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        backgroundColor: Colors.blue[600],
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-              // style: Theme(data: data, child: child),
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            ElevatedButton(
+              onPressed: someFunc,
+              onLongPress: longFunc,
+              child: const Text("Press this awesome button"),
             ),
-            Text(
-              _error_msg,
-              style: Theme.of(context).textTheme.bodyLarge,
+            const SizedBox(height: 30),
+            const Text("This is some lorem ipsum text"),
+            // const SizedBox(height: 10),
+            const Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Text(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              // width: MediaQuery.of(context).size.width * 0.9,
+              height: 180,
+              child: Image.asset("assets/images/tf-img1.jpg"),
+            )
+            // Image.asset("assets/images/tf-img1.jpg"),
           ],
         ),
       ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          FloatingActionButton(
-            onPressed: _decrementCounter,
-            tooltip: "Decrement",
-            child: const Icon(Icons.remove),
-          ),
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: "Increment",
-            child: const Icon(Icons.add),
-          ),
-        ],
-      ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
+
+      // body: Center(
+      //   child: SizedBox(
+      //     // width: MediaQuery.of(context).size.width * 0.9,
+      //     height: MediaQuery.of(context).size.height * 0.9,
+      //     child: Image.asset("assets/images/tf-img2.jpg"),
+      //   ),
+      // ),
+
+      // body: Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     crossAxisAlignment: CrossAxisAlignment.end,
+      //     children: [
+      //       TextButton(
+      //         onPressed: someFunc,
+      //         child: const Text("Text button"),
+      //       ),
+      //       ElevatedButton(
+      //         onPressed: someFunc,
+      //         child: const Text("Elevated button"),
+      //       ),
+      //       OutlinedButton(
+      //         onPressed: someFunc,
+      //         child: const Text("Outlined new button"),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+
+      // body: Center(
+      //   child: ElevatedButton(
+      //     style: ElevatedButton.styleFrom(
+      //       backgroundColor: Colors.red[700],
+      //       shape: RoundedRectangleBorder(
+      //         borderRadius: BorderRadius.circular(10),
+      //       ),
+      //     ),
+      //     onPressed: someFunc,
+      //     onLongPress: longFunc,
+      //     child: const Text(
+      //       "Elevated button",
+      //       style: TextStyle(
+      //         color: Colors.orange,
+      //       ),
+      //     ),
+      //   ),
+      // ),
+
+      // body: TextButton(
+      //   child: const Text("Press this button"),
+      //   onPressed: () => someFunc(),
+      //   onLongPress: () => longFunc(),
+      // ),
     );
   }
 }
 
 
-// import 'package:flutter/material.dart';
 
-// void main() => runApp(MyApp());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import "package:flutter/material.dart";
+
+// void main() => runApp(const MyApp());
 
 // class MyApp extends StatelessWidget {
 //   const MyApp({super.key});
 
 //   @override
 //   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//           appBar: AppBar(
-//         title: const Text("Some Title"),
-//       )),
-//       // home: Text("Some Text"),
+//     return const MaterialApp(
+//       title: "This is title",
+//       home: MyHomeHomePage(),
 //     );
 //   }
 // }
+
+// class MyHomeHomePage extends StatefulWidget {
+//   const MyHomeHomePage({super.key});
+
+//   @override
+//   State<MyHomeHomePage> createState() => _MyHomeHomePageState();
+// }
+
+// class _MyHomeHomePageState extends State<MyHomeHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.amber,
+//         title: const Text("Some Title"),
+//       ),
+//       body: Center(
+//         child: Container(
+//           width: 100,
+//           height: 100,
+//           color: Colors.red[900],
+//           child: const Center(
+//             child: Text("This is container's text"),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // import 'package:flutter/material.dart';
+
+// // void main() {
+// //   runApp(const MyApp());
+// // }
+
+// // class MyApp extends StatelessWidget {
+// //   const MyApp({super.key});
+
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return MaterialApp(
+// //       title: 'new App, understood?',
+// //       theme: ThemeData(
+// //         primarySwatch: Colors.blue,
+// //       ),
+// //       debugShowCheckedModeBanner: false,
+// //       home: const MyHomePage(),
+// //     );
+// //   }
+// // }
+
+// // class MyHomePage extends StatefulWidget {
+// //   const MyHomePage({super.key});
+
+// //   // final String title;
+
+// //   @override
+// //   State<MyHomePage> createState() => _MyHomePageState();
+// // }
+
+// // class _MyHomePageState extends State<MyHomePage> {
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Scaffold(
+// //       appBar: AppBar(
+// //         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+// //         title: Text("New Title abcd testing"),
+// //       ),
+// //       body: Center(
+// //         child: Container(
+// //           width: 100,
+// //           height: 100,
+// //           color: Colors.amber,
+// //           child: const Text("Not centered text"),
+// //           // child: const Center(
+// //           //   child: Text("Centered Text"),
+// //           // ),
+// //         ),
+// //       ),
+// //     );
+// //   }
+// // }
---
+++ lib/new.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+void main() {
+  print("Name: ");
+
+  var name = stdin.readLineSync();
+  print("Welcome: $name");
+}
