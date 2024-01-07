113f007c92b5fd33789698e6d9ec70179ff7bac3
Mohammed-taherali <mohammedtaherali5253@gmail.com>
2024-01-07 15:48:05 +0530

    learning scroll view

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: "App Title Goes here",
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(title: "Very new good title"),
     );
   }
@@ -39,34 +40,244 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.blue[600],
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            ElevatedButton(
-              onPressed: someFunc,
-              onLongPress: longFunc,
-              child: const Text("Press this awesome button"),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // element 1
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 200,
+                        width: 100,
+                        decoration: const BoxDecoration(
+                          color: Colors.amber,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        height: 200,
+                        width: 100,
+                        decoration: const BoxDecoration(
+                          color: Colors.black,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        height: 200,
+                        width: 100,
+                        decoration: const BoxDecoration(
+                          color: Colors.brown,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        height: 200,
+                        width: 100,
+                        decoration: const BoxDecoration(
+                          color: Colors.brown,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        height: 200,
+                        width: 100,
+                        decoration: const BoxDecoration(
+                          color: Colors.brown,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        height: 200,
+                        width: 100,
+                        decoration: const BoxDecoration(
+                          color: Colors.brown,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                    ],
+                  ),
+                ),
+                // element 2
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 200,
+                  decoration: const BoxDecoration(
+                    color: Colors.lightBlue,
+                  ),
+                ),
+                // element 3
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 200,
+                  decoration: const BoxDecoration(
+                    color: Colors.pinkAccent,
+                  ),
+                ),
+                // element 4
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 200,
+                  decoration: const BoxDecoration(
+                    color: Colors.greenAccent,
+                  ),
+                ),
+                // element 5
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 200,
+                  decoration: const BoxDecoration(
+                    color: Colors.red,
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 30),
-            const Text("This is some lorem ipsum text"),
-            // const SizedBox(height: 10),
-            const Padding(
-              padding: EdgeInsets.all(30.0),
-              child: Text(
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-              ),
-            ),
-            const SizedBox(height: 10),
-            SizedBox(
-              // width: MediaQuery.of(context).size.width * 0.9,
-              height: 180,
-              child: Image.asset("assets/images/tf-img1.jpg"),
-            )
-            // Image.asset("assets/images/tf-img1.jpg"),
-          ],
+          ),
         ),
       ),
+
+      // learning SingleChildScrollView
+      // body: SingleChildScrollView(
+      //   child: Center(
+      //     child: Column(
+      //       mainAxisAlignment: MainAxisAlignment.start,
+      //       children: [
+      //         ElevatedButton(
+      //           onPressed: someFunc,
+      //           onLongPress: longFunc,
+      //           child: const Text("Press this awesome button"),
+      //         ),
+      //         const SizedBox(height: 30),
+      //         const Text("This is some lorem ipsum text"),
+      //         // const SizedBox(height: 10),
+      //         const Padding(
+      //           padding: EdgeInsets.all(30.0),
+      //           child: Text(
+      //             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      //           ),
+      //         ),
+      //         const SizedBox(height: 10),
+      //         SizedBox(
+      //           // width: MediaQuery.of(context).size.width * 0.9,
+      //           // height: 180,
+      //           child: Image.asset("assets/images/tf-img1.jpg"),
+      //         )
+      //         // Image.asset("assets/images/tf-img1.jpg"),
+      //       ],
+      //     ),
+      //   ),
+      // ),
+
+      // body: Center(
+      //   child: Row(
+      //     children: [
+      //       Container(
+      //         padding: const EdgeInsets.all(10),
+      //         decoration: BoxDecoration(
+      //           border: Border.all(
+      //             width: 3,
+      //             color: Colors.red,
+      //           ),
+      //           color: Colors.orange,
+      //         ),
+      //         child: const Text("Centered text with padding"),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+
+      // body: const Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     crossAxisAlignment: CrossAxisAlignment.center,
+      //     children: [
+      //       Row(
+      //         mainAxisAlignment: MainAxisAlignment.center,
+      //         children: [
+      //           Text(
+      //             "[0][0]",
+      //             style: TextStyle(
+      //               fontWeight: FontWeight.bold,
+      //               backgroundColor: Colors.green,
+      //               color: Colors.orange,
+      //             ),
+      //           ),
+      //           SizedBox(width: 50),
+      //           Text("[0][1]"),
+      //           SizedBox(width: 50),
+      //           Text("[0][2]"),
+      //         ],
+      //       ),
+      //       SizedBox(
+      //         height: 50,
+      //       ),
+      //       Row(
+      //         mainAxisAlignment: MainAxisAlignment.center,
+      //         children: [
+      //           Text("[1][0]"),
+      //           SizedBox(width: 50),
+      //           Text("[1][1]"),
+      //           SizedBox(width: 50),
+      //           Text("[1][2]"),
+      //         ],
+      //       ),
+      //     ],
+      //   ),
+      // ),
+
+      // body: Column(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   crossAxisAlignment: CrossAxisAlignment.center,
+      //   children: [
+      //     Container(),
+      //     const Row(
+      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       children: [
+      //         Text("Prep"),
+      //         SizedBox(width: 10),
+      //         Text("Cook"),
+      //         SizedBox(width: 10),
+      //         Text("Feeds"),
+      //       ],
+      //     ),
+      //     const SizedBox(height: 10),
+      //     const Row(
+      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       children: [
+      //         Text("Prep"),
+      //         SizedBox(width: 10),
+      //         Text("Cook"),
+      //         SizedBox(width: 10),
+      //         Text("Feeds"),
+      //       ],
+      //     ),
+      //   ],
+      // ),
 
       // body: Center(
       //   child: SizedBox(
@@ -125,24 +336,6 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // import "package:flutter/material.dart";
 
 // void main() => runApp(const MyApp());
@@ -187,31 +380,6 @@ class _MyHomePageState extends State<MyHomePage> {
 //     );
 //   }
 // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // // import 'package:flutter/material.dart';
 
